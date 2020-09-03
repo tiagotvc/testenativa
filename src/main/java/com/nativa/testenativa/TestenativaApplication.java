38e7bd92d746f24c57fdd6cc24c3d438eb19e55d
Tiago Carvalho <carvalho110591@gmail.com>
2020-09-03 05:50:35 -0300

    Primeiro criado

---
+++ src/main/java/com/nativa/testenativa/TestenativaApplication.java
@@ -0,0 +1,13 @@
+package com.nativa.testenativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestenativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestenativaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nativa/testenativa/TestenativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nativa.testenativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestenativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
